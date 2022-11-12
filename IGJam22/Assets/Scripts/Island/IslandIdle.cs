90bdf3f0d84227cc453d236ac3b2e5283296c16d
Burningfoot <kevin.dors@mail.de>
2022-11-12 01:17:29 +0100

    Island Idle scripts

--- IGJam22/Assets/Scripts/Island/IslandIdle.cs
+++ IGJam22/Assets/Scripts/Island/IslandIdle.cs
@@ -6,36 +6,71 @@ using UnityEngine;
 public class IslandIdle : MonoBehaviour
 {
     private IslandIdleDirection direction;
+    private Vector3 startRotation;
+    private bool backRotation = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+        startRotation = transform.up;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (gameObject.transform.rotation.eulerAngles.magnitude > 5f)
+        if (backRotation && Vector3.Angle(startRotation, gameObject.transform.up) == 0)
         {
+            backRotation = false;
+        }
+        if (Vector3.Angle(startRotation , gameObject.transform.up) > 5 && !backRotation)
+        {
+            int randomInt = Random.Range(0, 3);
+
+            switch (randomInt)
+            {
+                case 0:
+                    direction = IslandIdleDirection.North;
+                    break;
+                case 1:
+                    direction = IslandIdleDirection.South;
+                    break;
+                case 2:
+                    direction = IslandIdleDirection.West;
+                    break;
+                case 3:
+                    direction = IslandIdleDirection.East;
+                    break;
+                default:
+                    break;
+            }
+
 
         }
-        switch (direction)
+
+        if (backRotation == false)
         {
-            case IslandIdleDirection.North:
-                gameObject.transform.Rotate(Vector3.right);
-                break;
-            case IslandIdleDirection.South:
-                gameObject.transform.Rotate(-Vector3.right);
-                break;
-            case IslandIdleDirection.West:
-                gameObject.transform.Rotate(Vector3.forward);
-                break;
-            case IslandIdleDirection.East:
-                gameObject.transform.Rotate(-Vector3.forward);
-                break;
-            default:
-                break;
+            switch (direction)
+            {
+                case IslandIdleDirection.North:
+                    gameObject.transform.Rotate(Vector3.right * Time.deltaTime, Space.World);
+                    break;
+                case IslandIdleDirection.South:
+                    gameObject.transform.Rotate(-Vector3.right * Time.deltaTime, Space.World);
+                    break;
+                case IslandIdleDirection.West:
+                    gameObject.transform.Rotate(Vector3.forward * Time.deltaTime, Space.World);
+                    break;
+                case IslandIdleDirection.East:
+                    gameObject.transform.Rotate(-Vector3.forward * Time.deltaTime, Space.World);
+                    break;
+                default:
+                    break;
+            }
         }
+        else
+        {
+            gameObject.transform.Rotate(Vector3.up * Time.deltaTime, Space.World);
+        }
+ 
 
 
     }
