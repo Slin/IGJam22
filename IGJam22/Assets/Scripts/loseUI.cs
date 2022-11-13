e9b3a3bd5e42e816a8e503b92bc8d0ed56f6a9dc
Nils Daumann <nils_daumann@slindev.com>
2022-11-13 17:13:00 +0100

    Game start and end.

---
+++ IGJam22/Assets/Scripts/loseUI.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class loseUI : MonoBehaviour
+{
+    private TikiSettlers settlers;
+    private RawImage image;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        image = GetComponent<RawImage>();
+        settlers = FindObjectOfType<TikiSettlers>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         image.enabled = settlers.didLoose;
+    }
+}
